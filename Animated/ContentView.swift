1b3fa9f4c5d8b37a853398a7d0154d172bbe792a
emptyhead <korea6370@yu.ac.kr>
2023-09-01 00:42:42 +0900

    2020/9/1 update

--- Animated/ContentView.swift
+++ Animated/ContentView.swift
@@ -8,16 +8,3 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews:
-    PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
--- Animated/OnboardingView.swift
+++ Animated/OnboardingView.swift
@@ -10,49 +10,85 @@ import RiveRuntime
 
 struct OnboardingView: View {
     let button = RiveViewModel(fileName: "button")
+    @State var showModal = false
     
     var body: some View {
         ZStack {
             background
             
-            VStack(alignment: .leading, spacing: 16) {
-                Text("Learn design & code")
-                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
-                    .frame(width: 260, alignment: .leading)
-                
-                Text("When I’m away, I will remember how you kissed me Under the lamppost back on Sixth street Hearing you whisper through the phone, Wait for me to come home")
-                    .customFont(.body)
-                    .opacity(0.7)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Spacer()
-                    
-                
-                button.view()
-                    .frame(width: 236, height: 64)
+            content
+                .offset(y: showModal ? -50 : 0)
+            
+            Color("Shadow")
+                .opacity(showModal ? 0.4 : 0)
+                .ignoresSafeArea()
+            
+            if showModal {
+                SignInView()
+                    .transition(.move(edge: .top).combined(with: .opacity))
                     .overlay(
-                        Label("Start the course", systemImage: "arrow.forward")
-                            .offset(x: 4, y: 4)
-                            .font(.headline)
+                        Button {
+                            withAnimation(.spring()) {
+                                showModal = false
+                            }
+                        } label: {
+                            Image(systemName: "xmark")
+                                .frame(width: 36, height: 36)
+                                .foregroundColor(.black)
+                                .background(.white)
+                                .mask(Circle())
+                            .shadow(color: Color("Shadow").opacity(0.3), radius: 5, x: 0, y: 3)
+                        }
+                            .frame(maxHeight: .infinity, alignment: .bottom)
                     )
-                    .background(
-                        Color.black
-                            .cornerRadius(30)
-                            .blur(radius: 10)
-                            .opacity(0.3)
-                            .offset(y: 10)
-                    )
-                    .onTapGesture {
-                        try? button.play(animationName: "active")
-                }
-                
-                Text("I have faith in what I see Now I know I have met an angel in person And she looks perfect, no I don't deserve this You look perfect tonight")
-                    .customFont(.footnote)
-                    .opacity(0.7)
+                    .zIndex(1)
             }
-            .padding(40)
-            .padding(.top, 40)
         }
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Learn design & code")
+                .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                .frame(width: 260, alignment: .leading)
+            
+            Text("When I’m away, I will remember how you kissed me Under the lamppost back on Sixth street Hearing you whisper through the phone, Wait for me to come home")
+                .customFont(.body)
+                .opacity(0.7)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+                
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the course", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    try? button.play(animationName: "active")
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                        withAnimation(.spring()) {
+                            showModal = true
+                        }
+                    }
+            }
+            
+            Text("I have faith in what I see Now I know I have met an angel in person And she looks perfect, no I don't deserve this You look perfect tonight")
+                .customFont(.footnote)
+                .opacity(0.7)
+        }
+        .padding(40)
+        .padding(.top, 40)
     }
     
     var background: some View {
